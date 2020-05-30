bd72aa797af36436aed4d6c1d3aa9ef4964448b8
Justin McCarthy <uvatmvf@gmail.com>
2020-05-30 13:49:14 -0600

    Removing bad git ignore.

---
+++ ToDoList/TodoGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Timers;
+
+namespace ToDoList
+{
+    public class TodoGenerator : IDisposable
+    {
+        private IServiceProvider _serviceProvider;
+        private Timer _timer;
+
+        public TodoGenerator(IServiceProvider services)
+        {
+            _serviceProvider = services;
+            _timer = new Timer(2500);
+            _timer.Elapsed += Timer_Elapsed;
+            _timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var todo = (NotifierService)_serviceProvider.GetService(typeof(NotifierService));
+            todo.Update(string.Empty, 0);
+        }
+
+        public void Dispose()
+        {
+            _timer.Elapsed -= Timer_Elapsed;
+            _timer.Stop();
+        }
+    }
+}
