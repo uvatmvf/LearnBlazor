252d6200354a7a6f7bc4064b6636275f273ec4b7
JMcCarthy <uvatmvf@gmail.com>
2022-06-10 12:56:51 -0600

    Add bunit test

--- BlazorComponent.Tests/PersonComponentTests.cs
+++ BlazorComponent.Tests/PersonComponentTests.cs
@@ -1,11 +1,17 @@
+using Bunit;
+using MvcWebApp.Components;
+
 namespace BlazorComponent.Tests
 {
     public class PersonComponentTests
     {
         [Fact]
-        public void Test1()
+        public void ComponentRendersAsExpected()
         {
+            using var context = new TestContext();
+            var classUnderTest = context.RenderComponent<PersonComponent>();
 
+            Assert.NotEmpty(classUnderTest.Markup);
         }
     }
 }
