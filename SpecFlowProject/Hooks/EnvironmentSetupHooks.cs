a03bdd6f2dd6da6e6af048ab1e6cd1d157f930ab
Justin McCarthy <uvatmvf@gmail.com>
2022-06-13 14:12:55 -0600

    Add specflow for mvc application (#4)

---
+++ SpecFlowProject/Hooks/EnvironmentSetupHooks.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using MvcWebApp;
+
+namespace SpecFlowProject.Hooks
+{
+    [Binding]
+    public class EnvironmentSetupHooks
+    {
+        private static IHost _host;
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            _host = Program.CreateHostBuilder(null).UseContentRoot(Path.Combine(Environment.CurrentDirectory, "../../../../MvcWebApp")).Build();
+
+            _host.Start();
+        }
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            _host.StopAsync().Wait();
+        }
+    }
+}
---
+++ SpecFlowProject/Hooks/TestExecutionHooks.cs
@@ -0,0 +1,14 @@
+﻿using SpecFlowProject.PageObjects;
+
+namespace SpecFlowProject.Hooks
+{
+    [Binding]
+    public class TestExecutionHooks
+    {
+        [BeforeScenario]
+        public void BeforeScenario(PersonPage personPage)
+        {
+            personPage.Goto();
+        }
+    }
+}
